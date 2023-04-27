c2d752d742fb194d68419c2193b424a1bc3c09da
pbarker <pbarker@buckley.org>
2023-04-27 08:16:21 -0700

    First commit to the local git directory

---
+++ NewProjects/PracticeClassFiles/MrB/ReviewOfProjects/src/Main.java
@@ -0,0 +1,11 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press ⌥⏎ with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("This is to test the right way to create in IntelliJ and then ");
+        System.out.println("commit and push to the github directory");
+
+    }
+}
