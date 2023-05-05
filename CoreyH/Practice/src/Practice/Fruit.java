3fcb1fe942429c9ac375aafcbd21cf26e97a9f70
chardemion <chardemion@buckley.org>
2023-05-05 10:59:25 -0700

    Package practice ana a main class called Practice

---
+++ CoreyH/Practice/src/Practice/Fruit.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Fruit {
+    //This is just practice class construction
+
+    String[] fruits = {"Strawberry", "Rasberry", "Blueberry", "Blackberry", "Elderberry", "Boysenberry"};
+
+public static void main (String[] args){
+    for (imt i = 0; i < fruits.length; i++){
+    }
+    System.out.println(fruits[int]);
+    }
+
+
+
+}
