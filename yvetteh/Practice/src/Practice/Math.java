7c5fc4035b92f311ec164cf8a6d8cecca44e8d94
yhettig <yhettig@buckley.org>
2023-05-05 11:07:16 -0700

    Math

---
+++ yvetteh/Practice/src/Practice/Math.java
@@ -0,0 +1,24 @@
+package Practice;
+//this is review of add and subtract
+public class Math {
+
+    public int addition(int x, int y){
+        int z;
+        z = x + y;
+        return z;
+
+    }
+
+    public int subtract(int x, int y){
+        int z;
+        z = x - y;
+        return z;
+    }
+
+    public static void main(String[] args){
+
+        Math e = new Math();
+        System.out.println(e.addition(6, 23));
+        System.out.println(e.addition(4, -10));
+    }
+}
