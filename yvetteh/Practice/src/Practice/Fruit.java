57d20bd80e68f6d2a9c5668827b695d809394257
yhettig <yhettig@buckley.org>
2023-05-05 10:51:36 -0700

    package practice and a main class called Practice

---
+++ yvetteh/Practice/src/Practice/Fruit.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class Fruit {
+    //this is just practice class construction
+
+    public static void main(String[] args){
+        String[] fruits = {"Strawberry", "Raspberry", "Blueberry", "Elderberry", "Boysenberry"};
+        for (int i = 0; i < fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+
+    }
+
+}
