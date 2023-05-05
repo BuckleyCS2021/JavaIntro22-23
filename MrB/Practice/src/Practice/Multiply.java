0a24bf9f4d755e3a53ba86a9635f0e71e9dd1c93
pbarker <pbarker@buckley.org>
2023-05-05 11:16:29 -0700

    Multiply

---
+++ MrB/Practice/src/Practice/Multiply.java
@@ -0,0 +1,27 @@
+package Practice;
+
+public class Multiply {
+    int value1, value2;
+
+    Multiply(){
+
+    }
+
+    Multiply(int x, int y){
+        value1 = x;
+        value2 = y;
+
+    }
+
+    public int Multiply(){
+        int z = value1 * value2;
+        return z;
+    }
+
+    public static void main(String[] args){
+
+        Multiply e = new Multiply(10, 10);
+        System.out.println(e.Multiply());
+    }
+
+}
