eff581ddb4073e783609b5c131bcf42ed2238693
habdo <habdo@buckley.org>
2023-05-11 14:20:19 -0700

    committed to main

--- HenryA/Practice/src/Practice/Fruit.java
+++ HenryA/Practice/src/Practice/Fruit.java
@@ -14,36 +14,78 @@ public class Fruit {
 
         double bank_balance = reader.nextDouble();
 
-        System.out.println("What fruit do you want to buy, we have: " + fruit[0] + ", " + fruit[1] + ", " + fruit[2] + ", " + fruit[3] + ", " + fruit[4] + ", " + fruit[5] + ", " + fruit[6] + ", and " + fruit[7]);
+        boolean running = true;
 
-        String fruit_choice = reader.next();
+        while (running) {
 
-        int fruit_price = (int) (Math.random() * amounts.length);
+            System.out.println("What fruit do you want to buy, we have: " + fruit[0] + ", " + fruit[1] + ", " + fruit[2] + ", " + fruit[3] + ", " + fruit[4] + ", " + fruit[5] + ", " + fruit[6] + ", and " + fruit[7]);
 
-        System.out.println(fruit_choice + " cost " + amounts[fruit_price] + ". How many would you like to buy?");
+            String fruit_choice = reader.next();
 
-        double fruit_num = reader.nextInt();
+            int fruit_price = (int) (Math.random() * amounts.length);
 
-        double final_cost = (amounts[fruit_price] *= fruit_num);
+            System.out.println(fruit_choice + " cost " + amounts[fruit_price] + ". How many would you like to buy?");
 
-        System.out.println("Your final amount is $" + final_cost + " Would you like to buy? (type 'y' for yes and 'n' for no)");
-        String y_or_n = reader.next();
+            double fruit_num = reader.nextInt();
 
-        double amount_needed = (bank_balance -= final_cost);
-        if (amount_needed < 0) {
-            amount_needed *= -1;
-        }
+            double final_cost = (amounts[fruit_price] *= fruit_num);
 
-        if (y_or_n.equals("y")) {
-            if (final_cost > bank_balance) {
-                System.out.println("You can't afford this! You need $" + amount_needed + " more dollars. Try again later");
+            System.out.println("Your final amount is $" + final_cost + " Would you like to buy? (type 'y' for yes and 'n' for no)");
+            String y_or_n = reader.next();
+
+            double amount_needed = (bank_balance -= final_cost);
+            if (amount_needed < 0) {
+                amount_needed *= -1;
             }
-            if (final_cost < bank_balance) {
-                System.out.println("Thank you for shopping! You now have $" + amount_needed + " left in your bank account!");
+
+            if (y_or_n.equals("y")) {
+                if (final_cost > bank_balance) {
+                    System.out.println("You can't afford this! You need $" + amount_needed + " more dollars. Try again later");
+                    System.out.println("Would you like to continue shopping? (yes or no) ");
+
+                    String continue_shopping = reader.next();
+
+                    if (continue_shopping.equals("yes")) {
+                        continue;
+                    }
+
+                    if (continue_shopping.equals("no")) {
+                        System.out.println("Thank you anyways!");
+                        running = false;
+                    }
+                }
+                if (final_cost < bank_balance) {
+                    System.out.println("Thank you for shopping! You now have $" + amount_needed + " left in your bank account!");
+                    boolean continute_shopping_loop = true;
+                    while (continute_shopping_loop) {
+                        System.out.println("Would you like to continue shopping? (yes or no) ");
+
+                        String continue_shopping = reader.next();
+
+                        if (continue_shopping.equals("yes")) {
+                            continute_shopping_loop = false;
+                            continue;
+                        }
+
+                        if (continue_shopping.equals("no")) {
+                            System.out.println("Thank you anyways!");
+                            continute_shopping_loop = false;
+                            running = false;
+                        }
+
+                        else {
+                            System.out.println("That's not an acceptable answer, please type exactly whats in the perentheses");
+                        }
+                    }
+                }
             }
+
+            if (y_or_n.equals("n")) {
+                System.out.println("Looser");
+            }
+
         }
-        if (y_or_n.equals("n")) {
-            System.out.println("Looser");
-        }
+
+
     }
 }
