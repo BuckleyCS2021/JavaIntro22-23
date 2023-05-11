e33cbfd6c9a98c389db3a064826b45372c21a6c6
habdo <habdo@buckley.org>
2023-05-11 14:28:42 -0700

    committed to main

---
+++ HenryA/Practice/src/Practice/Loops.java
@@ -0,0 +1,14 @@
+package Practice;
+public class Loops {
+
+    public static void main (String[] args) {
+
+        int i = 100;
+        while (i > 0) {
+            System.out.println("I is " + i);
+            i --;
+        }
+
+    }
+
+}
