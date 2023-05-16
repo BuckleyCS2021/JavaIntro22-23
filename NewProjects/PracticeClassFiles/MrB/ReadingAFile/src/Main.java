04a02591117892d15d65859dd4c81670f2989b64
sgtpjbarker <pbarker@buckley.org>
2023-05-16 11:22:05 -0700

    Read a file

---
+++ NewProjects/PracticeClassFiles/MrB/ReadingAFile/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // Use Scanner to read in from file
+        Scanner in = new Scanner(new File("data.csv"));
+
+        // Loop as long as file has a next line
+        while (in.hasNextLine()) {
+            // Store entire line in variable
+            String line = in.nextLine();
+
+            // Create an array with all the values in it, split by ","
+            String[] info = line.split(",");
+
+            // Print the array to see that it works
+            System.out.println(Arrays.toString(info));
+        }
+    }
+}
