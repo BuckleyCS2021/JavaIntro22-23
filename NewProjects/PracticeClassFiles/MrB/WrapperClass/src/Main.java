199bcc0897b368368f675311624dc8574252f44e
sgtpjbarker <pbarker@buckley.org>
2023-09-22 11:09:14 -0700

    Example of the wrapper resolved implementation

---
+++ NewProjects/PracticeClassFiles/MrB/WrapperClass/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static int timesTwo (int n) {
+        return n+2;
+    }
+    public static void main(String[] args) {
+        Integer val = 10;
+        int result1 = timesTwo(val);
+        Integer result2 = result1;
+        System.out.println(result2);
+        }
+    }
